4e9283e5fdfb926fc6b79d63c4befde008941415
Dominik <dominik@skylarkcreative.co.uk>
2023-04-19 10:08:33 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
+import { Box, OrbitControls, OrthographicCamera, Plane, Segment, Segments, Text, Tube, } from "@react-three/drei";
+
+
 
 function App() {
+  const [mousePosition, setMousePosition] = useState([0, 0]);
+
+  const points = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+    [0, 2, 1],
+  ];
+
+  const planeRef = useRef();
+
+  // Size of the plane
+  const planeWidth = 500;
+  const planeHeight = 500;
+
+  // Number of rows and columns
+  const numCols = 30;
+  const numRows = 30;
+
+  // Size of each cell
+  const cellWidth = planeWidth / numCols;
+  const cellHeight = planeHeight / numRows;
+
+  // Create row lines
+  const rowLines = [];
+  for (let i = 0; i <= numRows; i++) {
+    const y = i * cellHeight - planeHeight / 2;
+    rowLines.push(<Segment start={[-planeWidth / 2, y, 0]} end={[planeWidth / 2, y, 0]} />);
+  }
+
+  // Create column lines
+  const colLines = [];
+  for (let i = 0; i <= numCols; i++) {
+    const x = i * cellWidth - planeWidth / 2;
+    colLines.push(<Segment start={[x, -planeHeight / 2, 0]} end={[x, planeHeight / 2, 0]} />);
+  }
+
+  function handleMouseMove(event) {
+    const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
+    setMousePosition([mouseX, mouseY]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas orthographic camera={{ zoom: 10 }} style={{ width: '100vw', height: '100vh' }} onMouseMove={handleMouseMove}>
+      <OrthographicCamera makeDefault position={[0, 0, 100]} />
+      <Plane ref={planeRef} args={[planeWidth, planeHeight]}>
+        <meshBasicMaterial attach="material" color="#000" />
+      </Plane>
+
+      <Segments>
+        {rowLines.map((rowLine, rowIndex) => {
+          const angle = Math.atan2(
+            mousePosition[1] - (rowIndex * cellHeight - planeHeight / 2),
+            mousePosition[0],
+          );
+          console.log("ANGLE", angle)
+          return (
+            <>
+              <Segment
+                key={rowIndex}
+                start={rowLine.props.start}
+                end={rowLine.props.end}
+                rotation={[Math.PI / 2, 0, Math.PI / angle]}
+              />
+            </>
+          );
+        })}
+        {colLines.map((colLine, colIndex) => {
+          const angle = Math.atan2(
+            mousePosition[1],
+            mousePosition[0] - (colIndex * cellWidth - planeWidth / 2),
+          );
+          return (
+            <Segment
+              key={colIndex}
+              start={colLine.props.start}
+              end={colLine.props.end}
+              rotation={[0, 0, angle - Math.PI / 2]}
+            />
+          );
+        })}
+      </Segments>
+    </Canvas>
   );
 }
-
 export default App;
